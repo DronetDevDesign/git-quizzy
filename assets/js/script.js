2d5acc7087a6d1be14193613bc47dd7b1a2f90cc
Ron Dronet <ron@dronetdevdesign.com>
2022-06-18 14:36:17 -0500

    declaring variables containing quiz questions and possible answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,76 @@
 // THEN I can save my initials and score
 
 var userScore = 0;
-var timer = 100000;
+var timerStartNumber = 100;
 var startQuizBtn = document.querySelector("#start-quiz");
 
+// variable objects containing quiz questions and possible answers:
+var quizQuestionOne = {
+    askOne: "Commonly used data types DO not include:",
+    answersOne: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers",
+    ],
+};
+// console.log(quizQuestionOne);
+
+var quizQuestionTwo = {
+    askTwo: "The condition in an if / else statement is enclosed with _______.",
+    answersTwo: [
+      "quotes",
+      "curly brackets",
+      "parenthesis",
+      "square brackets",
+    ],
+};
+// console.log(quizQuestionTwo);
+
+var quizQuestionThree = {
+    askThree: "Arrays in JavaScript can be used to store _______.",
+    answersThree: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+};
+// console.log(quizQuestionThree);
+
+var quizQuestionFour = {
+    askFour: "String values must be enclosed within _______ when being assigned to variables.",
+    answersFour: [
+      "commas",
+      "curly brackets",
+      "quotes",
+      "parenthesis",
+    ],
+};
+// console.log(quizQuestionFour);
+
+var quizQuestionFive = {
+    askFive: "A very useful tool during development and debugging for printing content to the debugger is:",
+    answersFive: [
+      "JavaScript",
+      "terminal/zsh",
+      "for loops",
+      "console.log",
+    ],
+};
+
+var finalScore = userScore;
+// console.log(finalScore);
+
+var questionSlidesArray = [quizQuestionOne, quizQuestionTwo, quizQuestionThree, quizQuestionFour, quizQuestionFive];
+console.log(questionSlidesArray);
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function startQuiz() {
+  for (i = 0; i <= questionSlidesArray.length; i++) {
 
-  passwordText.value = password;
-
+  }
 }
 
 // Add event listener to generate button
-startQuizBtn.addEventListener("click", writePassword);
+startQuizBtn.addEventListener("click", startQuiz);
