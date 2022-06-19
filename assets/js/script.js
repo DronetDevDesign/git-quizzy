b0862bc46829bc1de57722e11a701b8f4c0a5ac9
Ron Dronet <ron@dronetdevdesign.com>
2022-06-19 18:33:57 -0500

    revised JS files and HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,89 +14,20 @@
 var userScore = 0;
 var timerNumber = document.getElementById("timer-number");
 var startQuizBtn = document.getElementById("start-quiz-btn");
-var count = 100;
+var count = 5;
+var heading1 = document.getElementById("slide-titles");
 
-// Write password to the #password input
-function startQuiz() {
-  
-}
+
 
 // Add event listener to button that starts timer:
 startQuizBtn.addEventListener("click", function() {
-startQuizBtn = setInterval(function() {
-    count -= 1;
-    timerNumber.textContent = count;
-    if (count <= 0) {
-    clearInterval(startQuizBtn);
-    }
-  }, 1000);
-});
+  // startQuizBtn.style.display = "none";
+  startQuizBtn = setInterval(function() {
+      count -= 1;
+      timerNumber.textContent = count;
+        if (count <= 0) {
+      clearInterval(startQuizBtn);
+        }
+    }, 1000);
+  });
 
-
-
-
-
-
-
-
-
-// variable objects containing quiz questions and possible answers:
-// var quizQuestionOne = {
-//     askOne: "Commonly used data types DO not include:",
-//     answersOne: [
-//       "strings",
-//       "booleans",
-//       "alerts",
-//       "numbers",
-//     ],
-// };
-// console.log(quizQuestionOne);
-
-// var quizQuestionTwo = {
-//     askTwo: "The condition in an if / else statement is enclosed with _______.",
-//     answersTwo: [
-//       "quotes",
-//       "curly brackets",
-//       "parenthesis",
-//       "square brackets",
-//     ],
-// };
-// console.log(quizQuestionTwo);
-
-// var quizQuestionThree = {
-//     askThree: "Arrays in JavaScript can be used to store _______.",
-//     answersThree: [
-//       "numbers and strings",
-//       "other arrays",
-//       "booleans",
-//       "all of the above",
-//     ],
-// };
-// console.log(quizQuestionThree);
-
-// var quizQuestionFour = {
-//     askFour: "String values must be enclosed within _______ when being assigned to variables.",
-//     answersFour: [
-//       "commas",
-//       "curly brackets",
-//       "quotes",
-//       "parenthesis",
-//     ],
-// };
-// console.log(quizQuestionFour);
-
-// var quizQuestionFive = {
-//     askFive: "A very useful tool during development and debugging for printing content to the debugger is:",
-//     answersFive: [
-//       "JavaScript",
-//       "terminal/zsh",
-//       "for loops",
-//       "console.log",
-//     ],
-// };
-
-// var finalScore = userScore;
-// console.log(finalScore);
-
-// var questionSlidesArray = [quizQuestionOne, quizQuestionTwo, quizQuestionThree, quizQuestionFour, quizQuestionFive];
-// console.log(questionSlidesArray);
