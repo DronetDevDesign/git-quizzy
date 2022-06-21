74bc43cf5e253f0f93f320bf443abe804d872855
Ron Dronet <ron@dronetdevdesign.com>
2022-06-21 11:30:20 -0500

    added eventListeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 
-var scoreCount = [0];
+var scoreCount = [];
 var finalScore = document.getElementById("score-number");
 var timerNumber = document.getElementById("timer-number");
 var startQuizBtn = document.getElementById("start-quiz-btn");
@@ -32,15 +32,15 @@ var btn10 = document.getElementById("three-btn-10");
 var btn11 = document.getElementById("three-btn-11");
 var btn12 = document.getElementById("three-btn-12");
 // section four buttons:
-var btn13 = document.getElementById("three-btn-13");
-var btn14 = document.getElementById("three-btn-14");
-var btn15 = document.getElementById("three-btn-15");
-var btn16 = document.getElementById("three-btn-16");
+var btn13 = document.getElementById("four-btn-13");
+var btn14 = document.getElementById("four-btn-14");
+var btn15 = document.getElementById("four-btn-15");
+var btn16 = document.getElementById("four-btn-16");
 // section five buttons:
-var btn17 = document.getElementById("three-btn-17");
-var btn18 = document.getElementById("three-btn-18");
-var btn19 = document.getElementById("three-btn-19");
-var btn20 = document.getElementById("three-btn-20");
+var btn17 = document.getElementById("five-btn-17");
+var btn18 = document.getElementById("five-btn-18");
+var btn19 = document.getElementById("five-btn-19");
+var btn20 = document.getElementById("five-btn-20");
 // section six input field:
 var enterYourInitials = document.getElementById("characters-for-initials");
 var submitInitialsBtn = document.getElementById("submit-initials");
@@ -61,6 +61,7 @@ startQuizBtn.addEventListener("click", function() {
     }, 1000);
   });
 
+// section one buttons:
 btn1.addEventListener("click", function() {
   count = count -10;
 });
@@ -79,6 +80,109 @@ btn4.addEventListener("click", function() {
   count = count -10;
 });
 
+// section two buttons:
+btn5.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn6.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn7.addEventListener("click", function() {
+  scoreCount = finalScore;
+  scoreCount = 10;
+  console.log(scoreCount);
+});
+
+btn8.addEventListener("click", function() {
+  count = count -10;
+});
+
+// section three buttons:
+btn9.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn10.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn11.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn12.addEventListener("click", function() {
+  scoreCount = finalScore;
+  scoreCount = 10;
+  console.log(scoreCount);
+});
+
+// section four buttons:
+btn13.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn14.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn15.addEventListener("click", function() {
+  scoreCount = finalScore;
+  scoreCount = 10;
+  console.log(scoreCount);
+});
+
+btn16.addEventListener("click", function() {
+  count = count -10;
+});
+
+// section five buttons:
+btn17.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn18.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn19.addEventListener("click", function() {
+  count = count -10;
+});
+
+btn20.addEventListener("click", function() {
+  scoreCount = finalScore;
+  scoreCount = 10;
+  console.log(scoreCount);
+  return finalScore;
+});
+
+
+
+// section six input field:
+enterYourInitials.addEventListener("click", function() {
+
+});
+
+// submitInitialsBtn.addEventListener("click", function() {
+//   var newSubmitInitialsBtn = document.getElementById("submit-initials").value;
+//   if(localStorage.getItem("data") === null) {
+//     localStorage.setItem("data", "[]");
+//   };
+
+//   submitInitialsBtn = JSON.parse(localStorage.getItem('data'));
+//   submitInitialsBtn.push(newSubmitInitialsBtn);
+//   localStorage.setItem("data", JSON.stringify(submitInitialsBtn));
+// });
+
+// section seven buttons:
+goBackBtn.addEventListener("click", function() {
+
+});
+
+clearScoresBtn.addEventListener("click", function() {
+
+});
 
 
 
